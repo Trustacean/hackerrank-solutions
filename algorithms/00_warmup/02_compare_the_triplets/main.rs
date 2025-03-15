80a9894de9baa5d95f0dbc50900abcca5951fe20
Edward Vito <pizzacrustacean15@gmail.com>
2025-03-15 15:38:42 +0700

    solved and added compare the triplets

---
+++ algorithms/00_warmup/02_compare_the_triplets/main.rs
@@ -0,0 +1,63 @@
+use std::cmp::Ordering;
+use std::env;
+use std::fs::File;
+use std::io::{self, BufRead, Write};
+
+/*
+ * Complete the 'compareTriplets' function below.
+ *
+ * The function is expected to return an INTEGER_ARRAY.
+ * The function accepts following parameters:
+ *  1. INTEGER_ARRAY a
+ *  2. INTEGER_ARRAY b
+ */
+
+ fn compare_triplets(a: &[i32], b: &[i32]) -> Vec<i32> {
+    let mut scores = vec![-0, 0];
+    for i in 0..a.len() {
+        match a[i].cmp(&b[i]) {
+            Ordering::Less => scores[1] += 1,
+            Ordering::Greater => scores[0] += 1,
+            Ordering::Equal => (),
+        }
+    }
+    scores
+}
+
+
+fn main() {
+    let stdin = io::stdin();
+    let mut stdin_iterator = stdin.lock().lines();
+
+    let mut fptr = File::create(env::var("OUTPUT_PATH").unwrap()).unwrap();
+
+    let a: Vec<i32> = stdin_iterator
+        .next()
+        .unwrap()
+        .unwrap()
+        .trim_end()
+        .split(' ')
+        .map(|s| s.to_string().parse::<i32>().unwrap())
+        .collect();
+
+    let b: Vec<i32> = stdin_iterator
+        .next()
+        .unwrap()
+        .unwrap()
+        .trim_end()
+        .split(' ')
+        .map(|s| s.to_string().parse::<i32>().unwrap())
+        .collect();
+
+    let result = compare_triplets(&a, &b);
+
+    for i in 0..result.len() {
+        write!(&mut fptr, "{}", result[i]).ok();
+
+        if i != result.len() - 1 {
+            write!(&mut fptr, " ").ok();
+        }
+    }
+
+    writeln!(&mut fptr).ok();
+}
