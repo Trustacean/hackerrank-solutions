1211fdcd5387eafc817c6d1c48288bd01cc2e128
Edward Vito <pizzacrustacean15@gmail.com>
2025-03-22 18:08:27 +0700

    solved and added birthday cake candles

---
+++ algorithms/00_warmup/07_birthday_cake_candles/main.rs
@@ -0,0 +1,53 @@
+use std::env;
+use std::fs::File;
+use std::io::{self, BufRead, Write};
+
+/*
+ * Complete the 'birthdayCakeCandles' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts INTEGER_ARRAY candles as parameter.
+ */
+
+fn birthday_cake_candles(candles: &[i32]) -> i32 {
+    let mut max = 0;
+    let mut count = 0;
+    for &i in candles {
+        if i == max {
+            count += 1;
+        }
+        if max < i {
+            max = i;
+            count = 1;
+        }
+    }
+    count
+}
+
+fn main() {
+    let stdin = io::stdin();
+    let mut stdin_iterator = stdin.lock().lines();
+
+    let mut fptr = File::create(env::var("OUTPUT_PATH").unwrap()).unwrap();
+
+    let _candles_count = stdin_iterator
+        .next()
+        .unwrap()
+        .unwrap()
+        .trim()
+        .parse::<i32>()
+        .unwrap();
+
+    let candles: Vec<i32> = stdin_iterator
+        .next()
+        .unwrap()
+        .unwrap()
+        .trim_end()
+        .split(' ')
+        .map(|s| s.to_string().parse::<i32>().unwrap())
+        .collect();
+
+    let result = birthday_cake_candles(&candles);
+
+    writeln!(&mut fptr, "{}", result).ok();
+}
