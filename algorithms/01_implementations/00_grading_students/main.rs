e731e64559273663172b00f58f216436059c0420
Edward Vito <pizzacrustacean15@gmail.com>
2025-03-24 15:59:04 +0700

    solved and added grading students

---
+++ algorithms/01_implementations/00_grading_students/main.rs
@@ -0,0 +1,51 @@
+use std::env;
+use std::fs::File;
+use std::io::{self, BufRead, Write};
+
+/*
+ * Complete the 'gradingStudents' function below.
+ *
+ * The function is expected to return an INTEGER_ARRAY.
+ * The function accepts INTEGER_ARRAY grades as parameter.
+ */
+
+fn grading_students(grades: &[i32]) -> Vec<i32> {
+    let mut final_grade : Vec<i32> = Vec::new();
+    for &i in grades {
+        let diff = 5 - (i % 5);
+        if i < 38 || diff >=3 {
+            final_grade.push(i);
+        } else {
+             final_grade.push(i + diff);
+        }
+    }
+    final_grade
+}
+
+fn main() {
+    let stdin = io::stdin();
+    let mut stdin_iterator = stdin.lock().lines();
+
+    let mut fptr = File::create(env::var("OUTPUT_PATH").unwrap()).unwrap();
+
+    let grades_count = stdin_iterator.next().unwrap().unwrap().trim().parse::<i32>().unwrap();
+
+    let mut grades: Vec<i32> = Vec::with_capacity(grades_count as usize);
+
+    for _ in 0..grades_count {
+        let grades_item = stdin_iterator.next().unwrap().unwrap().trim().parse::<i32>().unwrap();
+        grades.push(grades_item);
+    }
+
+    let result = grading_students(&grades);
+
+    for i in 0..result.len() {
+        write!(&mut fptr, "{}", result[i]).ok();
+
+        if i != result.len() - 1 {
+            writeln!(&mut fptr).ok();
+        }
+    }
+
+    writeln!(&mut fptr).ok();
+}
